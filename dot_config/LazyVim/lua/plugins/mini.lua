2d8b2bd6f1330e363e6dc9162cf08ad6958b77a5
Prescott Murphy <prurph@gmail.com>
2023-11-24 19:40:21 -0500

    Add mini.bracketed with remaps around LazyVim defaults

--- dot_config/LazyVim/lua/plugins/mini.lua
+++ dot_config/LazyVim/lua/plugins/mini.lua
@@ -1,9 +1,38 @@
--- ~ Configuration for echasnovski/mini.nvim plugins
+local function suff(suffix, opts)
+  return {
+    suffix = suffix,
+    options = opts or {},
+  }
+end
+
+local bracketed = {
+  "echasnovski/mini.bracketed",
+  opts = {
+    buffer = suff("b"),
+    -- Keep [c for classes
+    comment = suff("z"),
+    conflict = suff("x"),
+    diagnostic = suff("d"),
+    -- Keep [f for functions (treesitter config); "" disables
+    file = suff(""),
+    indent = suff("i"),
+    jump = suff("j"),
+    location = suff("l"),
+    oldfile = suff("o"),
+    quickfix = suff("q"),
+    -- Keep [t for todos. Use n for "nodes"
+    treesitter = suff("n"),
+    undo = suff("u"),
+    window = suff("w"),
+    yank = suff("y"),
+  },
+}
 
 local surround = {
   "echasnovski/mini.surround",
   opts = {
     mappings = {
+      -- surround.vim-like mappings: operator first instead of default that uses gz(a/d/c)
       add = "ys",
       delete = "ds",
       replace = "cs",
@@ -12,5 +41,6 @@ local surround = {
 }
 
 return {
+  bracketed,
   surround,
 }
