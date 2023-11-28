88ab74e6eaf123cd20862c8a4b5096bc95ad0921
Prescott Murphy <prurph@gmail.com>
2023-11-27 20:21:32 -0500

    Add image support

--- dot_config/LazyVim/lua/plugins/neo-tree.lua
+++ dot_config/LazyVim/lua/plugins/neo-tree.lua
@@ -1,5 +1,12 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    -- Requires image magick and magick luarock. Install luarocks then:
+    -- 1. Install magick: `luarocks install magick --lua-version 5.1
+    -- 2. Add luarocks to package.path as seen in lua/config/options.lua with a few package.path = ... commands
+    -- 3. Set up image.nvim as below. Must pass opts or it doesn't get initialized by LazyVim(?)
+    { "3rd/image.nvim", opts = {} },
+  },
   opts = {
     default_component_configs = {
       symbols = {
@@ -19,6 +26,14 @@ return {
     source_selector = {
       winbar = true,
       statusline = true,
+    },
+    window = {
+      mappings = {
+        ["P"] = {
+          "toggle_preview",
+          config = { use_float = true, use_image_nvim = true },
+        },
+      },
     },
   },
 }
