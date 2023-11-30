9dc2739d67173d286927ff54e5f76dee91adf961
Prescott Murphy <prurph@gmail.com>
2023-11-29 19:52:43 -0500

    Add oil.nvim

---
+++ dot_config/LazyVim/lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+  opts = {},
+}
