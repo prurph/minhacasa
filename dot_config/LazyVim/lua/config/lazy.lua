23a23ad8ce4a0df66157167564b72d91e7a6918b
Prescott Murphy <prurph@gmail.com>
2023-12-01 18:35:26 -0500

    Add python-semshi

--- dot_config/LazyVim/lua/config/lazy.lua
+++ dot_config/LazyVim/lua/config/lazy.lua
@@ -15,6 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.python-semshi" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
