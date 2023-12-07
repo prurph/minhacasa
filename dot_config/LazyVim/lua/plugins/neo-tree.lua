ea624b0f7b78cf7ced15c334aa29a3258db6c173
Prescott Murphy <prurph@gmail.com>
2023-12-07 18:12:26 -0500

    Explicitly pass 3rd/image.nvim default opts and clarify comment on disabled plugin

--- dot_config/LazyVim/lua/plugins/neo-tree.lua
+++ dot_config/LazyVim/lua/plugins/neo-tree.lua
@@ -4,8 +4,40 @@ return {
     -- Requires image magick and magick luarock. Install luarocks then:
     -- 1. Install magick: `luarocks install magick --lua-version 5.1
     -- 2. Add luarocks to package.path as seen in lua/config/options.lua with a few package.path = ... commands
-    -- 3. Set up image.nvim as below. Must pass opts or it doesn't get initialized by LazyVim(?)
-    { "3rd/image.nvim", opts = {} },
+    -- 3. Set up image.nvim as below. Must explicitly pass default opts or it doesn't get initialized properly.
+    --
+    "MunifTanjim/nui.nvim",
+    {
+      "3rd/image.nvim",
+      opts = {
+        backend = "kitty",
+        integrations = {
+          markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+          },
+          neorg = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            filetypes = { "norg" },
+          },
+        },
+        max_width = nil,
+        max_height = nil,
+        max_width_window_percentage = nil,
+        max_height_window_percentage = 50,
+        window_overlap_clear_enabled = false, -- toggles images when windows are overlapped
+        window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+        editor_only_render_when_focused = false, -- auto show/hide images when the editor gains/looses focus
+        tmux_show_only_in_active_window = false, -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+        hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
+      },
+    },
   },
   opts = {
     default_component_configs = {
--- dot_config/LazyVim/lua/plugins/nvim-lsp-file-operations.lua
+++ dot_config/LazyVim/lua/plugins/nvim-lsp-file-operations.lua
@@ -4,6 +4,12 @@ return {
     "antosha417/nvim-lsp-file-operations",
     -- Disabled because it causes nvim to exit with status 134, which makes chezmoi always report as such.
     -- https://github.com/antosha417/nvim-lsp-file-operations/issues/24
+    -- The issue doesn't seem to be with this plugin per se, but something with the way everything gets
+    -- loaded in LazyVim seems to cause 3rd/image.nvim to work properly (images can be previewed) but
+    -- nvim always exits with 134.
+    --
+    -- I'm able to repro this behavior _without_ nvim-lsp-file-operations by passing an empty opts table
+    -- to 3rd/image.nvim, so I wonder if somehow the above setup causes that to happen.
     enabled = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
